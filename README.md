a1a47403129b4f99112ab77b59a19091f15d91c1
Justin Littman <justinlittman@stanford.edu>
2023-06-30 15:04:28 -0400

    Initial gem commit.

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
+[![Gem Version](https://badge.fury.io/rb/mais-orcid-client.svg)](https://badge.fury.io/rb/mais-orcid-client)
+[![CircleCI](https://circleci.com/gh/sul-dlss/mais-orcid-client.svg?style=svg)](https://circleci.com/gh/sul-dlss/mais-orcid-client)
+[![Maintainability](https://api.codeclimate.com/v1/badges/5919e7ae4cd162861585/maintainability)](https://codeclimate.com/github/sul-dlss/mais-orcid-client/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/5919e7ae4cd162861585/test_coverage)](https://codeclimate.com/github/sul-dlss/mais-orcid-client/test_coverage)
+
 # mais-orcid-client
 API client for accessing MAIS's ORCID endpoints.
+
+## Installation
+
+Install the gem and add to the application's Gemfile by executing:
+
+    $ bundle add mais-orcid-client
+
+If bundler is not being used to manage dependencies, install the gem by executing:
+
+    $ gem install mais-orcid-client
+
+## Usage
+
+For one-off requests:
+
+```ruby
+require "mais_orcid/client"
+
+# NOTE: The settings below live in the consumer, not in the gem.
+client = MaisOrcid::Client.configure(
+  client_id: Settings.mais_orcid.client_id,
+  client_secret: Settings.mais_orcid.client_secret,
+  base_url: Settings.mais_orcid.base_url,
+  transfer_endpoint_id: Settings.globus.transfer_endpoint_id
+)
+client.fetch_orcid_user(sunetid: 'nataliex')
+```
+
+You can also invoke methods directly on the client class, which is useful in a
+Rails application environment where you might initialize the client in an
+initializer and then invoke client methods in many other contexts where you want
+to be sure configuration has already occurred, e.g.:
+
+```ruby
+# config/initializers/mais-orcid-client.rb
+MaisOrcid::Client.configure(
+  client_id: Settings.mais_orcid.client_id,
+  client_secret: Settings.mais_orcid.client_secret,
+  base_url: Settings.mais_orcid.base_url,
+  transfer_endpoint_id: Settings.globus.transfer_endpoint_id
+)
+
+# app/services/my_mais_orcid_service.rb
+# ...
+def create_user_directory
+  MaisOrcid::Client.fetch_orcid_user(sunetid: 'nataliex')
+end
+# ...
+```
+
+## Development
+
+After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+
+To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and the created tag, and push the `.gem` file to [rubygems.org](https://rubygems.org).
---
+++ lib/mais_orcid/client.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+require "active_support/core_ext/module/delegation"
+require "active_support/core_ext/hash/indifferent_access"
+
+require "faraday"
+require "faraday/retry"
+require "oauth2"
+require "singleton"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.new
+loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
+loader.push_dir(File.absolute_path("#{__FILE__}/../.."))
+loader.setup
+
+module MaisOrcid
+  # Client for interacting with MAIS's ORCID API
+  class Client
+    include Singleton
+
+    # Struct for ORCID-relating data returned from API.
+    OrcidUser = Struct.new(:sunetid, :orcidid, :scope, :access_token, :last_updated) do
+      def update?
+        scope.include?("/activities/update")
+      end
+    end
+
+    class << self
+      # @param client_id [String] the client identifier registered with Globus
+      # @param client_secret [String] the client secret to authenticate with Globus
+      # @param base_url [String] the base URL for the API
+      def configure(client_id:, client_secret:, base_url:)
+        instance.base_url = base_url
+        instance.client_id = client_id
+        instance.client_secret = client_secret
+        self
+      end
+
+      delegate :fetch_orcid_users, :fetch_orcid_user, to: :instance
+    end
+
+    attr_accessor :base_url, :client_id, :client_secret
+
+    # @param [int] limit number of users requested
+    # @param [int] page_size number of users per page
+    # @return [Array<OrcidUser>] orcid users
+    def fetch_orcid_users(limit: nil, page_size: nil)
+      orcid_users = []
+      next_page = first_page(page_size)
+      loop do
+        response = get_response(next_page)
+        response[:results].each do |result|
+          orcid_users << OrcidUser.new(result[:sunet_id], result[:orcid_id], result[:scope], result[:access_token], result[:last_updated])
+          return orcid_users if limit && limit == orcid_users.size
+        end
+        # Currently next is always present, even on last page. (This may be changed in future.)
+        next_page = response.dig(:links, :next)
+        return orcid_users if last_page?(response[:links])
+      end
+    end
+
+    # @param [string] sunet to fetch
+    # @return [<OrcidUser>, nil] orcid user or nil if not found
+    def fetch_orcid_user(sunetid:)
+      result = get_response("/users/#{sunetid}", allow404: true)
+
+      return if result.nil?
+
+      OrcidUser.new(result[:sunet_id], result[:orcid_id], result[:scope], result[:access_token], result[:last_updated])
+    end
+
+    private
+
+    def first_page(page_size)
+      path = "/users?scope=ANY"
+      path += "&page_size=#{page_size}" if page_size
+      path
+    end
+
+    def last_page?(links)
+      links[:self] == links[:last]
+    end
+
+    def get_response(path, allow404: false)
+      response = conn.get("/mais/orcid/v1#{path}")
+
+      return if allow404 && response.status == 404
+
+      raise "UIT MAIS ORCID User API returned #{response.status}" if response.status != 200
+
+      body = JSON.parse(response.body).with_indifferent_access
+      raise "UIT MAIS ORCID User API returned an error: #{response.body}" if body.key?(:error)
+
+      body
+    end
+
+    # @return [Faraday::Connection]
+    def conn
+      @conn ||= begin
+        conn = Faraday.new(url: base_url) do |faraday|
+          faraday.request :retry, max: 3,
+            interval: 0.5,
+            interval_randomness: 0.5,
+            backoff_factor: 2
+        end
+        conn.options.timeout = 500
+        conn.options.open_timeout = 10
+        conn.headers[:user_agent] = "stanford-library-sul-pub"
+        conn.headers[:authorization] = token
+        conn
+      end
+    end
+
+    def token
+      client = OAuth2::Client.new(client_id, client_secret, site: base_url,
+        token_url: "/api/oauth/token", authorize_url: "/api/oauth/authorize",
+        auth_scheme: :request_body)
+      token = client.client_credentials.get_token
+      "Bearer #{token.token}"
+    end
+  end
+end
